8925c05626167e048d193958de930cae64160fd3
saranya <saranyarushikesh.chinderker@broadcom.com>
2023-02-20 14:15:12 +0530

    pushing sample

---
+++ src/components/StatusTile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./StatusTile.module.scss";
+
+const StatusTile = () => {
+  return (
+    <div className={classes.container}>
+      Hello
+    </div>
+  )
+}
+
+export default StatusTile
